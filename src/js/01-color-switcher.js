40d9b6c90f77bca7d12cc24a616c9d80ce09317d
Yaroslav-ppp <83807204+Yaroslav-ppp@users.noreply.github.com>
2022-01-19 23:03:47 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+let timerId = null;
+
+refs.startBtn.addEventListener('click', () => {
+  timerId = setInterval(function onChangeColor() {
+    function getRandomHexColor() {
+      return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
+    const color = getRandomHexColor();
+    document.body.style.background = color;
+  }, 1000);
+  refs.startBtn.disabled = true;
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  console.log(`Interval with id ${timerId} has stopped!`);
+  refs.startBtn.disabled = false;
+});
